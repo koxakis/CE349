e9b9bfd8a450ddc3f9c2aa87b0a068489e2ec81b
Nikolas Koxenoglou <koxakin@gmail.com>
2017-03-19 20:59:42 +0200

    Added shellcode template and first (faild) attempt at creating a exectued stack

---
+++ hw1/shellcode.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main( int argc, char *argv[]){
+    
+    char *name[2];
+
+    name[0] = "/bin/sh";
+    name[1] = NULL;
+
+    execve(name[0], name, NULL);
+
+    exit(0);
+
+    return (0);
+} 
